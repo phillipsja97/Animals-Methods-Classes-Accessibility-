5f9c696207ce361352a3979b48e3aa5401cb1ba4
Jamie <phillipsja97@gmail.com>
2020-03-02 21:17:57 -0600

    Add project files.

---
+++ Animals/Program.cs
@@ -0,0 +1,27 @@
+﻿using Animals.Reptiles;
+using System;
+
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var americanAlligator = new Alligator(100);
+            americanAlligator.IsAquatic = true;
+
+            var africanAlligator = new Alligator(2);
+            africanAlligator.IsAquatic = false;
+
+            africanAlligator.Grunt();
+            americanAlligator.Grunt();
+            americanAlligator.Run(75);
+            africanAlligator.Run(4);
+            americanAlligator.Run(40);
+            
+            Console.ReadLine();
+        
+        }
+    }
+}
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    class Alligator
+    {
+        private int _stepGoal;
+        private int _stepCount;
+        public bool IsAquatic { get; set; }
+
+        public Alligator(int stepGoal) //constructor method
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+
+        }
+
+        public void Grunt()
+        {
+            if (IsAquatic)
+            {
+                Console.WriteLine("bubble, bubble, bubble");
+            } 
+            else
+            {
+            Console.WriteLine("grrrrr!");
+            }
+        }
+
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran this many steps: {steps}");
+            
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("You're a step master");
+            }
+        }
+    }
+}
